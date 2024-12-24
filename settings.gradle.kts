3db6ac187f81148a8cb4756c3adbedeb29b4e068
Sam Taiwo <staiwo@onlydates.fan>
2024-12-24 21:38:42 +0000

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+rootProject.name = "engine"
+rootProject.buildFileName = "engine.gradle.kts"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    includeBuild("Client")
+    includeBuild("Server")
+    includeBuild("KotlinGen")
+    includeBuild("Sample")
+    includeBuild("Shared")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
